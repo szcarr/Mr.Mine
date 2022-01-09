45535d5e6cf287e0f427b0216031367cb8c42372
szcarr <itwangzy@gmail.com>
2022-01-09 21:13:31 +0100

    New version

--- push.bat
+++ push.bat
@@ -3,6 +3,6 @@ git add .
 git commit -m "New version"
 git remote rm origin
 git remote add origin https://github.com/szcarr/Mr.Mine.git
-git pull origin master
-git push origin master
+git pull --rebase origin main
+git push origin main
 pause
